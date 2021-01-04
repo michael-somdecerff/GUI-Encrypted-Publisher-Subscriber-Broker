95983abd4a123e5c501e1468abac3ce0634a5a0a
Michael S <Frost2779@users.noreply.github.com>
2021-01-04 13:02:17 -0600

    Updated some small stuff

--- Common/Encryption/PublicPrivateEncryption.cs
+++ Common/Encryption/PublicPrivateEncryption.cs
@@ -4,6 +4,16 @@ using System.Text;
 
 namespace Common.Encryption {
     public static class PublicPrivateEncryption {
+        public static PublicPrivateEncryptionPair GenerateEncryptionPair() {
+            return null;
+        }
 
+        public static string Encode(string str, byte[] publicKey) {
+            return null;
+        }
+
+        public static string Decode(string str, byte[] privateKey) {
+            return null;
+        }
     }
 }
--- Common/Encryption/SymetricEncryptionPair.cs
+++ Common/Encryption/SymetricEncryptionPair.cs
@@ -1,13 +1,18 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Common.Encryption {
     public class SymetricEncryptionPair {
+        [JsonProperty]
         public byte[] SymetricKey { get; private set; }
+
+        [JsonProperty]
         public byte[] InitVector { get; private set; }
 
+        [JsonConstructor]
         public SymetricEncryptionPair(byte[] key, byte[] iv) {
             if (key == null)
                 throw new ArgumentNullException("Key argument is null");
